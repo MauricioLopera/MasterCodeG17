dc81434942c7f6c62551843108b7f4706f905c59
Mauricio Lopera <sistemas@helpex.co>
2022-05-30 21:27:03 -0500

    Inicio Modulo

---
+++ 02-IntroJS/01-SalidasEntradas/js/app.js
@@ -0,0 +1,23 @@
+//Formas de Mostrar Datos al Usuario
+
+//1.Console Log
+console.log('Hola Mundo'); //Recibe Texto
+console.log(1990); //Recibe Numeros
+console.log(2 + 4); //Recibe Operaciones
+console.log(10<3); //Recibe comparaciones
+
+//2.Document.Write
+document.write('Hola Mundo'); //Recibe lo que le mandes
+
+//3.Alert (popup)
+alert('Hola Mundo');
+
+/*Solicitar Datos
+al Usuario Final*/
+
+//1. Prompt (popup)
+prompt('Ingresa tu Nombre');
+
+/*La ejecucion de codigo se realiza
+del parentesis mas interior hacia afuera*/
+alert(prompt('Ingresa tu edad'));
