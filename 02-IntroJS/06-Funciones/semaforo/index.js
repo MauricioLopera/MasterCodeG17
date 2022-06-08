720c38c6b3df07307e4dadd7cbb44419388f2ab9
Gerardo Nava Pereda <gerardo.nava.pereda@gmail.com>
2022-06-08 18:03:55 -0500

    ✨ [Introducir] ejercicio del semáforo

--- 02-IntroJS/06-Funciones/semaforo/index.js
+++ 02-IntroJS/06-Funciones/semaforo/index.js
@@ -1,19 +1,59 @@
-// setInterval(cambiarLuz, 2000);
+// const colores = ["green", "red", "yellow"];
 
-// const colores = [];
-const colores = {
+/* const colores = {
   verde: "green",
   rojo: "red",
   amarillo: "yellow",
-};
+}; */
 
-function cambiarLuz(name) {
-  console.log("🚀 ~ file: index.js ~ line 6 ~ cambiarLuz ~ name", name);
-  const elem = document.getElementById(name);
-  console.log("🚀 ~ file: index.js ~ line 8 ~ cambiarLuz ~ elem", elem);
-  elem.style.backgroundColor = colores[name];
-  // Es posible agregar un atributo de estilo con un valor a un elemento, pero no se recomienda porque puede sobrescribir otras propiedades en el atributo de estilo.
+const ROJO = 3;
+const AMARILLO = 2;
+const VERDE = 1;
+
+const colorApagado = "#ececec";
+let color;
+let luzActual = -1;
+let siguienteLuz;
+let anteriorLuz;
+
+
+function cambiarLuz() {
+  switch (luzActual) {
+    case ROJO:
+      color = "green";
+      siguienteLuz = "verde";
+      anteriorLuz = "rojo";
+      luzActual = VERDE;
+      break;
+    case VERDE:
+      color = "yellow";
+      siguienteLuz = "amarillo";
+      anteriorLuz = "verde";
+      luzActual = AMARILLO;
+      break;
+    case AMARILLO:
+      color = "red";
+      siguienteLuz = "rojo";
+      anteriorLuz = "amarillo";
+      luzActual = ROJO;
+      break;
+    default:
+      color = "red";
+      siguienteLuz = "rojo";
+      luzActual = ROJO;
+      break;
+  }
+
+  const prederLuz = document.getElementById(siguienteLuz);
+  prederLuz.style.backgroundColor = color;
+
+  const apagarLuz = document.getElementById(anteriorLuz);
+  apagarLuz.style.backgroundColor = colorApagado;
+  /* Es posible agregar un atributo de estilo con un valor a un elemento con setAttribute,
+  pero no se recomienda porque puede sobrescribir las otras propiedades existentes,
+  en el atributo de estilo. */
   // elem.setAttribute("style", "background-color: red;");
 }
 
-cambiarLuz("rojo");
+setInterval(cambiarLuz, 2000);
+// cambiarLuz();
