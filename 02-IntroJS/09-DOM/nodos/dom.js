2bf6cc042f6740c30ab1fe7bbe1fb351b3143750
Gerardo Nava Pereda <gerardo.nava.pereda@gmail.com>
2022-06-09 19:29:42 -0500

    ✨ [Introducir] DOM nodos

---
+++ 02-IntroJS/09-DOM/nodos/dom.js
@@ -0,0 +1,23 @@
+// NODOS:
+// Elementos
+// Atributos
+// Texto
+
+// Seleccionar nodos
+const elemento = document.getElementById("cielo");
+console.log("🚀 ~ file: dom.js ~ line 8 ~ elemento", elemento);
+const elementos = document.getElementsByClassName("tierra");
+console.log("🚀 ~ file: dom.js ~ line 10 ~ elementos", elementos)
+const elemento2 = document.querySelector(".tierra");
+const elementos2 = document.querySelectorAll(".tierra");
+
+// document.getElementsByName
+// document.getElementByTagName
+
+// Crear nodos
+
+// Agregar nodos
+
+// Borrar nodos
+
+// Sustituirlos
