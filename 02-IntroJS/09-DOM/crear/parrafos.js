3ef30e3cd91cd293379646bae5ba43510e3ac50c
Gerardo Nava Pereda <gerardo.nava.pereda@gmail.com>
2022-06-14 21:35:14 -0500

    ✨ [Introducir] DOM

--- 02-IntroJS/09-DOM/crear/parrafos.js
+++ 02-IntroJS/09-DOM/crear/parrafos.js
@@ -1,7 +1,37 @@
-const parrafo = document.createElement("p");
+// Crear un parrafo
 
+const parrafo = document.createElement("p");
 const nodeDeTexto = document.createTextNode("Avatar La leyenda de Korra");
 parrafo.appendChild(nodeDeTexto);
 
+// parrafo.innerHTML("Avatar La leyenda de Korra");
+
+parrafo.style.color = "red"
+parrafo.style.fontFamily = "Arial"
+
 const padre = document.getElementById("padre");
 padre.appendChild(parrafo);
+
+// getters
+// setters
+
+{/* <a></a> */}
+const link = document.createElement("a");
+// "Ir a Google"
+const linkTexto = document.createTextNode("Ir a Google");
+{/* <a>Idiv.padre>ul>r a Google</a> */}
+link.appendChild(linkTexto);
+
+// link.click();
+
+// href=""
+const linkAtributo = document.createAttribute("href");
+// href="https://www.google.com"
+linkAtributo.value = "https://www.google.com";
+// <a href="https://www.google.com">Ir a Google</a>
+link.setAttributeNode(linkAtributo);
+padre.appendChild(link);
+
+// Remover elementos
+const elementoARemover = document.getElementById("remover")
+elementoARemover.remove();
--- 02-IntroJS/09-DOM/nodos/dom.js
+++ 02-IntroJS/09-DOM/nodos/dom.js
@@ -8,8 +8,19 @@ const elemento = document.getElementById("cielo");
 console.log("🚀 ~ file: dom.js ~ line 8 ~ elemento", elemento);
 const elementos = document.getElementsByClassName("tierra");
 console.log("🚀 ~ file: dom.js ~ line 10 ~ elementos", elementos)
+
+
 const elemento2 = document.querySelector(".tierra");
+
 const elementos2 = document.querySelectorAll(".tierra");
+
+
+const elementoDeListaRoja = document.querySelector("li[color='rojo']");
+console.log("🚀 ~ file: dom.js ~ line 19 ~ elementoDeListaRoja", elementoDeListaRoja);
+
+elementoDeListaRoja.style.color = "#ff0000";
+
+
 
 // document.getElementsByName
 // document.getElementByTagName
@@ -19,5 +30,7 @@ const elementos2 = document.querySelectorAll(".tierra");
 // Agregar nodos
 
 // Borrar nodos
+
+// removeElement
 
 // Sustituirlos
